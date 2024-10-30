799c7d275acb1c999e4427db240d10bc9ef6dc5a
XingLuoYunxiao <139027005+XingLuoYunxiao@users.noreply.github.com>
2024-10-30 20:55:35 +0800

    Delete products/default/.preview directory

--- products/default/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/features/Login/Index.ts
+++
@@ -1,2 +0,0 @@
-export { InfoMessagePage } from "@bundle:com.example.ictproject/default@Login/ets/pages/InfoMessagePage";
-export { CustomDialogComponent } from "@bundle:com.example.ictproject/default@Login/ets/view/CustomDialogComponent";
